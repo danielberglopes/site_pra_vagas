f6deb476042a98cd57badc9f9dad435febcd435c
Daniel Berg <105886779+danielberglopes@users.noreply.github.com>
2024-04-26 14:32:35 -0300

    Fiz a criação das tabelas de vagas
    Fiz a criação das tabelas de vagas pra cadastra as vagas

---
+++ teste-php2.md/database/migrations/2024_04_26_172216_create_vagas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vagas', function (Blueprint $table) {
+            $table->id();
+        $table->string(' nome_da_vaga',200);
+        $table->string('descricao',200);
+        $table->string('cidade',200);
+        $table->string('local',200);
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vagas');
+    }
+};
